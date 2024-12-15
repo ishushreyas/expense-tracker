c520940ebbd40232e0d7ac3ae3e774c9aab0840b
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2024-12-15 20:45:25 +0000

    use state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ExpenseTracker from './components/ExpenseTracker';
 import Login from './components/Login';
 
