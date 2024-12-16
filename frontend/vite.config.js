138cceeae3de5ab09373a6bb37747a78c2621423
Ishu Shreyas <ishushreyas@gmail.com>
2024-12-17 02:15:02 +0530

    Updated login checking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Expense Tracker',
-        short_name: 'ExpenseTracker',
-        description: 'Split bill among mates!',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
   ],
-});
+},
+define: {
+    'process.env': process.env, // Expose variables to the build
+  },);
